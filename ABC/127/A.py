6e298ad0b87e3bb8eb80ca0fd980b8b9d14e2688
nh <nh@mail.com>
2023-10-19 21:18:25 +0900

    ABC解答

--- ABC/127/A.py
+++ ABC/127/A.py
@@ -1,8 +1,7 @@
-A, B = map(int, input().split())
-
-if A >= 13:
-    print(B)
-elif 6 <= A and A <= 12:
-    print(B // 2)
+A,B=map(int,input().split())
+if A>=13:
+	print(B)
+elif 6<=A and A<=12:
+	print(B//2)
 else:
-    print(0)
+	print(0)
--- ABC/153/A.py
+++ ABC/153/A.py
@@ -1,15 +1,5 @@
-H, A = map(int, input().split())
-if H % A ==0:
-    print(H // A)
+H,A=map(int,input().split())
+if H%A==0:
+	print(H//A)
 else:
-    print(H // A + 1)
-
-# count = 0
-
-# while True:
-#     count += 1
-#     H = H - A
-
-#     if H <= 0:
-#         print(count)
-#         exit()
+	print(H//A+1)
